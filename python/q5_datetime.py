612d741ed55513b00fa8ce7e2c892c01570f3722
Orlando Torres <orlando@Orlandos-MacBook-Pro.local>
2017-03-10 19:05:10 -0800

    answers

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,24 @@
 # Hint:  use Google to find python function
+from datetime import date
 
-####a) 
+
+d0 = date(2013, 2, 1)
+d1 = date(2015, 7, 28)
+date1 = d1 - d0
+print(date1) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
-####b)  
+d0 = date(2013, 12, 31)
+d1 = date(2015, 5, 28)
+date2 = d1 - d0
+print(date2) 
 date_start = '12312013'  
 date_stop = '05282015'  
 
-####c)  
+d0 = date(1994, 1, 15)
+d1 = date(2015, 7, 14)
+date3 = d1 - d0
+print(date3)   
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
