a7090491678f4f82e78485b55b0c884dc74fe563
Orlando Torres <orlando@Orlandos-MBP.hsd1.ca.comcast.net>
2017-03-15 16:32:51 -0700

    added answers

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,19 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+from pandas import Series
+
+
+df = pd.read_csv(
+'/users/orlando/ds/metis/metisgh/prework/dsp/python/faculty.csv', delimiter=' *, *', engine='python'
+)
+
+
+emails = []
+
+for x in df['email']:
+    emails.append(x)
+
+
+emails_s = pd.Series(emails)
+
+emails_s.to_csv('/users/orlando/ds/metis/metisgh/prework/dsp/python/emails.csv', index=False)
+
