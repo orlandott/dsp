b1179b109ebb1747e513396ae29a3d0ff229df51
Orlando Torres <orlando@Orlandos-MBP.hsd1.ca.comcast.net>
2017-03-15 18:06:42 -0700

    added answers

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,27 @@
-PLACE YOUR CODE HERE
+import re
+import pandas as pd
+from pandas import Series
+import numpy as np
+
+########## Import csv and remove whitespace
+
+df = pd.read_csv(
+'/users/orlando/ds/metis/metisgh/prework/dsp/python/faculty.csv', delimiter=' *, *', engine='python'
+)
+
+######### Eliminate all variations of degree names (with and without periods)
+
+df.degree.replace((r"Ph\.*D\.*"), 'PhD', regex=True,inplace=True)
+df.degree.replace((r"Sc\.*D\.*"), 'ScD', regex=True,inplace=True)
+df.degree.replace((r"M\.*S\.*"), 'MS', regex=True,inplace=True)
+
+#########################################
+
+df['surname'] = 0
+surname = []
+for x in df['name']:
+	name = x.split(' ')
+	surname.append(name[-1])
+	df['surname'][x] = surname
+
+print (df['surname'])
